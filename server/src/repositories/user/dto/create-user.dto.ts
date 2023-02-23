bf8ed4687c68e20d879545f5e13312ac0242a222
realtrynna <realtrynna@gmail.com>
2023-02-23 22:50:49 +0900

    Feat: class-validator 기반의 Custom Decorator 구현

---
+++ server/src/repositories/user/dto/create-user.dto.ts
@@ -0,0 +1,38 @@
+import {
+    ApiHeader,
+    ApiProperty,
+    Assert,
+} from '../../../common/common-dto-validator';
+
+@ApiHeader({ name: '사용자 가입 데이터' })
+export class CreateUserDto {
+    @ApiProperty({
+        example: 'admin@admin.com',
+        type: String,
+        description: '이메일',
+    })
+    @Assert.IsNotEmpty()
+    @Assert.IsEmail()
+    email: string;
+
+    @ApiProperty({
+        example: '윤승근',
+        type: String,
+        description: '이름',
+    })
+    @Assert.IsNotEmpty()
+    @Assert.IsString()
+    @Assert.MinLength(2)
+    @Assert.MaxLength(6)
+    name: string;
+
+    /**
+     * @todo: 정규표현식(Matches) 사용
+     */
+    @ApiProperty({
+        example: 'admin1@',
+        type: String,
+        description: '비밀번호',
+    })
+    password: string;
+}
