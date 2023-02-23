85886c5cccabbb329861cec7277b3da2df7c1395
realtrynna <realtrynna@gmail.com>
2023-02-23 22:51:40 +0900

    Chore: 라이브러리 설치와 Prettier tabwidth 수정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { UserModule } from './entities/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath:
+                process.env.NODE_ENV === 'production'
+                    ? '.production.env'
+                    : process.env.NODE_ENV === 'stage'
+                    ? '.stage.env'
+                    : 'development.env',
+        }),
+        UserModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
