5f5edd38bc1a822756007320c6a51e4e1276c0f2
realtrynna <realtrynna@gmail.com>
2023-03-21 21:19:15 +0900

    feat: Multer builder pattern

--- server/src/common/builders/multer.builder.ts
+++ server/src/common/builders/multer.builder.ts
@@ -1,7 +1,37 @@
 import { Request } from 'express';
 
+import {
+    getMulterOption,
+    TAllowImageMimeTypes,
+} from '../../config/multer.config';
+
+export const ALLOW_IMAGE_TYPES: TAllowImageMimeTypes = [
+    'image/jpg',
+    'image/jpeg',
+    'image/png',
+];
 export class MulterBuilder {
+    readonly #allowTypes: Array<string> = [];
+
+    #resource = '';
+    #path = '';
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor() {}
 
-    allowImageType() {}
+    pushAllowTypes() {
+        this.#allowTypes.push(...ALLOW_IMAGE_TYPES);
+        return this;
+    }
+
+    setResource(data: string) {
+        this.#resource = data;
+        return this;
+    }
+
+    setPath(path: string) {
+        this.#path = path;
+        return this;
+    }
+
+    build(isProd: boolean) {}
 }
